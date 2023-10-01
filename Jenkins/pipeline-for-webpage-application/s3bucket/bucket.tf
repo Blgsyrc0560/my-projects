843dd67a1a545f35506b3a19bfac41c54eb01921
bekirokutan <blgsyrc0560@gmail.com>
2023-10-01 13:39:15 +0300

    jenkins project added and files changed

---
+++ Jenkins/pipeline-for-webpage-application/s3bucket/bucket.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "jenkins-project-backend-okutan"
+  force_destroy =true
+}
