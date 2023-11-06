44cda9bcecfe652009529d3395d7d9fd14945b95
Bekir OKUTAN <113426804+bekirokutan@users.noreply.github.com>
2023-11-06 16:02:03 +0300

    Update index.php

--- Kubernetes/Basic-Web-Application/index.php
+++ Kubernetes/Basic-Web-Application/index.php
@@ -1,13 +1,9 @@
 <?php
-//$servername = "34.234.91.79:3307";
-//$username = "bekir";
-//$password = "123456789";
-//$database = "mydatabase";
+$servername = "mysql-service";
+$username = "bekir";
+$password = "123456789";
+$database = "mydatabase";
 
-$servername = $_ENV['DB_HOST'];
-$username   = $_ENV['DB_USERNAME'];
-$password   = $_ENV['DB_PASSWORD'];
-$database   = $_ENV['DB_DATABASE'];
 
 // Connection
 $conn = new mysqli($servername, $username, $password, $database);
